057653a89e74cb4847e73d15688f40045eac2c22
youhyeon <youhyeon@wishket.com>
2017-03-22 19:32:45 +0900

    [player admin] safe delete action added

--- mysite/admin.py
+++ mysite/admin.py
@@ -18,12 +18,20 @@ class CharacterAdmin(admin.ModelAdmin):
     list_display = ('name', 'scenario', 'player', 'status')
 
 
+def delete_selected(modeladmin, request, queryset):
+    for player in queryset:
+        character = Character.objects.get(player=player)
+        character.status = Character.EMPTY
+        character.save()
+        player.delete()
+delete_selected.short_description = "삭제 및 캐릭터 상태 변경"
+
+
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'gender', 'is_adult', 'email', 'reg_date', 'modify_date')
-
+    actions = [delete_selected]
 
 @admin.register(Scenario)
 class ScenarioAdmin(admin.ModelAdmin):
     list_display = ('name', 'adult_only', 'gender_only', 'is_active')
-
