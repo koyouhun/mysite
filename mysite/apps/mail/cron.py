798186598e892632b2d519ade8808439f564b2b6
youhyeon <youhyeon@wishket.com>
2017-07-11 11:43:52 +0900

    dd

--- mysite/apps/mail/cron.py
+++ mysite/apps/mail/cron.py
@@ -9,9 +9,9 @@ def email_sending():
     mails = Mail.objects.all()
 
     for mail in mails:
-        data = {'player_name': mail.player_name,
-                'scenario_name': mail.scenario_name,
-                'player_reg_date': mail.player_reg_date.astimezone(timezone('Asia/Seoul')).strftime("%Y-%m-%d %H:%M")}
+        data = {'player_name': mail.player_name}
+                # 'scenario_name': mail.scenario_name,
+                # 'player_reg_date': mail.player_reg_date.astimezone(timezone('Asia/Seoul')).strftime("%Y-%m-%d %H:%M")}
 
         EmailMessage(
             '[게임데이] 신청 확인',
